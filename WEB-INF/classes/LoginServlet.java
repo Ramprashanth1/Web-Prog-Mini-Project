e453b663bc7b4da6efbbfc93d4c14a99985846dc
Ramprashanth <ramprashanth@student.tce.edu>
2023-04-06 07:16:19 +0530

    changes

--- WEB-INF/classes/LoginServlet.java
+++ WEB-INF/classes/LoginServlet.java
@@ -1,73 +1,139 @@
 import java.io.IOException;
-  import java.io.PrintWriter;
-  import javax.servlet.ServletConfig;
-  import javax.servlet.ServletException;
-  import javax.servlet.annotation.WebServlet;
-  import javax.servlet.http.HttpServlet;
-  import javax.servlet.http.HttpServletRequest;
-  import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
 
-  /**
-  * Servlet implementation class LoginServlet
-  */
-  @WebServlet("/LoginServlet")
-  public class LoginServlet extends HttpServlet {
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
-        
-    /**
-      * @see HttpServlet#HttpServlet()
-      */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
   /**
-    * @see Servlet#init(ServletConfig)
-    */
+   * @see HttpServlet#HttpServlet()
+   */
+  public LoginServlet() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @see Servlet#init(ServletConfig)
+   */
   public void init(ServletConfig config) throws ServletException {
     // TODO Auto-generated method stub
   }
 
   /**
-    * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-    */
-  protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String method= request.getMethod();
-    if(method.equals("GET"))
-    {
+   * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String method = request.getMethod();
+    if (method.equals("GET")) {
       doGet(request, response);
-    }
-    else if(method.equals("POST"))
-    {
+    } else if (method.equals("POST")) {
       doPost(request, response);
     }
   }
 
   /**
-    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-    */
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     // TODO Auto-generated method stub
     response.getWriter().append("Served at: ").append(request.getContextPath());
   }
 
   /**
-    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-    */
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     response.setContentType("text/html");
     PrintWriter out = response.getWriter();
     String username = request.getParameter("username");
-    String password = request.getParameter("password");
-    if(username.equals("satish") && password.equals("test123"))
-    {
-      out.println("Welcome Mr "+ username + " and your password is "+ password);
+    String password = request.getParameter("password").toString();
+
+    try {
+      Class.forName("oracle.jdbc.OracleDriver");
+      Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "tiger");
+
+      // Statement stmt = con.createStatement();
+      // ResultSet rs = stmt.executeQuery("select * from users");
+      String sql = "select * from users where u_name = ? and password = ?";
+
+      PreparedStatement stmt = con.prepareStatement(sql);
+      stmt.setString(1, username);
+      stmt.setString(2, password);
+      ResultSet rs = stmt.executeQuery();
+      // out.print(rs.getInt(1) + " " + rs.getString(2));
+      if (rs.next() == false) {
+        out.println("<html><body><h1>Invalid Username or Password</h1></body></html>");
+      } else {
+        out.println("<html><body><h1>Welcome " + rs.getString(2) + "</h1><br><h2>Your ID is " + rs.getString(1)
+            + "<br><a href='/'>Home</a></body></html>");
+      }
+
+      // step5 close the connection object
+      con.close();
+
+    } catch (Exception e) {
+
     }
-    else
-    {
-      out.println("I doubt you are an hacker.. pls run away");
-    }		
+
+    // try {
+    // Class.forName("oracle.jdbc.OracleDriver");
+    // Connection con =
+    // DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system",
+    // "sportsma");
+
+    // // Statement stmt=con.Statement();
+    // // ResultSet rs=stmt.executeQuery("select * from flight");
+
+    // String sql = "select * from users where u_name = ? and password = ?";
+
+    // PreparedStatement stmt = con.prepareStatement(sql);
+    // stmt.setString(1, username);
+    // stmt.setString(2, password);
+
+    // ResultSet rs = stmt.executeQuery();
+    // if (rs.next() == false)
+    // out.print(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+
+    // if (rs.next() == false) {
+    // out.println("I doubt you are an hacker.. pls run away");
+    // out.println("Welcome Mr " + username + " and your password is " + password);
+    // } else {
+    // }
+    // } catch (Exception e) {
+    // out.println(e);
+    // }
+    //
+    // step5 close the connection object
+    // con.close();
+    //
+    // } catch (SQLException e)
+    // {
+    //
+    // }
+
+    // if(username.equals("satish") && password.equals("test123"))
+    // {
+    // out.println("Welcome Mr "+ username + " and your password is "+ password);
+    // }
+    // else
+    // {
+    // out.println("I doubt you are an hacker.. pls run away");
+    // }
   }
 
-  }
+}
--- index.jsp
+++ index.jsp
@@ -1,3 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+pageEncoding="ISO-8859-1"%>
+<%@ page import="java.sql.*" %>
+<%! int count=0; %>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -46,24 +51,33 @@
           <ul>
             <li><a href="#">HOME</a></li>
             <li><a href="#">ABOUT</a></li>
-            <li><a href="#">Blog</a></li>
+            <li><a href="blog.xml">Blog</a></li>
             <li><a href="Login.jsp">Login</a></li>
             <li><a href="#">CONTACT</a></li>
           </ul>
         </div>
 
         <div class="search">
-          <form action=""HelloForm" method="Get">
+          <form action="index.jsp" method="post">
             <input
-            class="srch"
-            type="search"
-            name="title"
-            placeholder="Type To text"
+            class=""
+            type="text"
+            name="username"
+            placeholder="Type your Name"
           />
-          <input class ="btn" type="submit" value="Submit" />
+          <input class ="btn" type="submit" name="submit" value="Submit" />
           </form>
           <!--<a href="#"> <button class="btn">Search</button></a>-->
         </div>
+        <% 
+        String sub = request.getParameter("submit");
+        if(sub!=null)
+        {
+          String username = request.getParameter("username");
+          out.println("Welcome User, "+ username);
+          
+        }
+        %>
         <div class="darkmode">
           <a href="#"> <button class="btn">Dark Mode</button></a>
         </div>
@@ -81,16 +95,55 @@
           development determines how it functions.
         </p>
       </div>
+      <div>
+        <p>Users:</p>
+        <%
+        try
+{
+  
+ Class.forName("oracle.jdbc.OracleDriver");  
+ Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","tiger");  
+//Context initContext = new InitialContext();
+//Context envContext  = (Context)initContext.lookup("java:/comp/env");
+//DataSource ds = (DataSource)envContext.lookup("jdbc/myoracle");
+//Connection conn = ds.getConnection();
+Statement stmt=con.createStatement();  
+ResultSet rs=stmt.executeQuery("select * from users");  
+ 
+                                          
+        
+        
+ while(rs.next()){  
+out.print(rs.getString(1)+"  "+rs.getString(2)+"<br>");  
+  count=count+1;
+ }
+ out.print("Total Number Of Users : "+count+"<br>");
+//step5 close the connection object  
+con.close();  
+  
+} catch (SQLException e) { 
+        
+     } 
+
+
+%>
+      </div>
       <div class="container">
         <div class="carousel-caption">
-          <h3>To Read More Blog Click Below</h3>
+          <h3>To Learn More, Click Below</h3>
           <p id="show"></p>
           <p><a class="btn btn-lg btn-primary" href="#" id="learn" onclick="sendinfo()">Learn more</a></p>
         </div>
       </div>
+      <h1>Details</h1>  
+    <div >  
+    <b>To:</b> <span id="to"></span><br>  
+    <b>From:</b> <span id="from"></span><br>  
+    <b>Message:</b> <span id="message"></span>  
+    </div> 
     </div>
 	
-
+     
 	 <script>
       $(document).ready(function () {
         $(".darkmode").click(function () {
@@ -109,6 +162,23 @@
         xhttp.open("GET","ajax.txt");
         xhttp.send()
       }
+
+      var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
+    }
+};
+xhttp.open("GET", "test.xml", true);
+xhttp.send();
+
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+	//document.getElementByID("dom").innerHTML=xmlDoc;
+    document.getElementById("to").innerHTML= xmlDoc.getElementsByTagName("to")[0].childNodes[0].nodeValue;  
+document.getElementById("from").innerHTML= xmlDoc.getElementsByTagName("from")[0].childNodes[0].nodeValue;  
+document.getElementById("message").innerHTML= xmlDoc.getElementsByTagName("body")[0].childNodes[0].nodeValue;  
+}
     </script>
     <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
   	
