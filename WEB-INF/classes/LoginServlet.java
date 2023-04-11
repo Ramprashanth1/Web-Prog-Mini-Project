87fb4abcc6859f3421e0eba29846f398337eb0e7
Ramprashanth <ramprashanth@student.tce.edu>
2023-04-11 06:24:05 +0530

    java

--- WEB-INF/classes/LoginServlet.java
+++ WEB-INF/classes/LoginServlet.java
@@ -7,32 +7,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
- */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
-  /**
-   * @see HttpServlet#HttpServlet()
-   */
   public LoginServlet() {
     super();
-    // TODO Auto-generated constructor stub
+
   }
 
-  /**
-   * @see Servlet#init(ServletConfig)
-   */
   public void init(ServletConfig config) throws ServletException {
-    // TODO Auto-generated method stub
+
   }
 
-  /**
-   * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
-   *      response)
-   */
   protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     String method = request.getMethod();
@@ -43,19 +30,11 @@ public class LoginServlet extends HttpServlet {
     }
   }
 
-  /**
-   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-   *      response)
-   */
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    // TODO Auto-generated method stub
+
     response.getWriter().append("Served at: ").append(request.getContextPath());
   }
 
-  /**
-   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-   *      response)
-   */
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     response.setContentType("text/html");
     PrintWriter out = response.getWriter();
